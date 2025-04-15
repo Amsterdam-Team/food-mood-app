ce7cabf321096334aec45f63fe8cb5e222edb206
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-04-15 17:28:27 +0200

    edited names for clarification

--- src/main/kotlin/logic/GetIraqiMealsUseCase.kt
+++ src/main/kotlin/logic/GetIraqiMealsUseCase.kt
@@ -7,12 +7,12 @@ class GetIraqiMealsUseCase(
 ) {
     fun getOnlyIraqiMeals(): List<Meal>{
         return mealsRepository.getAllMeals()
-            .filter(::onlyHighQualityData)
+            .filter(::isHighQualityData)
             .filter { it.description!!.contains("Iraq", ignoreCase = true) ||
                 it.tags!!.contains("iraqi")
             }
     }
-    private fun onlyHighQualityData(meal: Meal): Boolean{
+    private fun isHighQualityData(meal: Meal): Boolean{
         return meal.description != null && meal.tags != null
     }
 }
--- src/main/kotlin/logic/GetItalianMealsForLargeGroupsUseCase.kt
+++ src/main/kotlin/logic/GetItalianMealsForLargeGroupsUseCase.kt
@@ -8,11 +8,11 @@ class GetItalianMealsForLargeGroupsUseCase(
 
     fun getItalianMealsForLargeGroups(): List<Meal>{
         return mealsRepository.getAllMeals()
-            .filter(::getOnlyHighQualityMeals)
+            .filter(::isHighQualityData)
             .filter { (it.tags!!.contains("italian") || it.name!!.contains("italian", ignoreCase = true) || it.description!!.contains("italy")) && it.tags!!.contains("for-large-groups") }
     }
 
-    private fun getOnlyHighQualityMeals(meal: Meal): Boolean{
+    private fun isHighQualityData(meal: Meal): Boolean{
         return meal.tags != null && meal.description != null && meal.name != null
     }
 }
