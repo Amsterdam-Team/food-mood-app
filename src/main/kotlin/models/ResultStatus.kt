bddace4429ecc12afe65e1217693a5f70784c091
mina0maher <mahermena160@gmail.com>
2025-04-15 10:38:09 +0200

    make result class properties accessible

--- src/main/kotlin/models/ResultStatus.kt
+++ src/main/kotlin/models/ResultStatus.kt
@@ -2,7 +2,7 @@ package org.example.models
 import kotlin.collections.List
 
 sealed class ResultStatus {
-    class Success(meals : List<Meal>): ResultStatus()
-    class Error(exception: Exception): ResultStatus()
-    class Loading(): ResultStatus()
+    class Success(val meals: List<Meal>) : ResultStatus()
+    class Error(val exception: Exception) : ResultStatus()
+    class Loading : ResultStatus()
 }
