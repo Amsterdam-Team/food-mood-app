2c6e172ff27fbf356b13aeda36178cf622d1748b
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-04-16 16:33:45 +0200

    keto meal helper use case

---
+++ src/main/kotlin/presentation/KetoMealHelperUI.kt
@@ -0,0 +1,46 @@
+package presentation
+
+import logic.models.Meal
+import org.example.logic.ketoMealHelper.GetRandomKetoMealsUseCase
+import presentation.uiController.BaseUIController
+import presentation.utils.tryToExecute
+
+class KetoMealHelperUI(private val getKetoFriendlyMealsUseCase: GetRandomKetoMealsUseCase) : BaseUIController {
+
+    private lateinit var suggestedMeal: Meal
+    private var ketoMeals = getKetoFriendlyMealsUseCase.getKetoMeals()
+    private var input: String = ""
+
+    override fun execute() {
+        tryToExecute(
+            action = { getKetoFriendlyMealsUseCase.getKetoMealRandomly(ketoMeals) },
+            onSuccess = { onGetRandomMealSuccess(it) }
+        )
+    }
+
+    private fun onGetRandomMealSuccess(meal: Meal) {
+        suggestedMeal = meal
+        println("Suggested Meal: ${meal.name}")
+        ketoMeals = ketoMeals.filterNot { it == meal }
+        handleUserInput()
+
+    }
+
+    private fun handleUserInput() {
+        println("press Y if you like it , or N if you do not like it")
+        input = readlnOrNull()?.trim()?.lowercase() ?: ""
+        when (input) {
+            "y" -> println(suggestedMeal.toString())
+            "n" -> {
+                execute()
+            }
+
+            else -> {
+                println("invalid input :(")
+                handleUserInput()
+            }
+        }
+
+    }
+
+}
