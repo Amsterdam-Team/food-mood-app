b419010e169ff54c2abcadadd1ce792b482c789f
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-04-16 21:04:18 +0300

    refactor exception handling: replace basic hierarchy with sealed class structure

--- src/main/kotlin/logic/exception/FoodMoodException.kt
+++ src/main/kotlin/logic/exception/FoodMoodException.kt
@@ -5,6 +5,8 @@ sealed class FoodMoodException() : Exception() {
         data object InvalidCalories : Validation()
         data object InvalidProtein : Validation()
         data object EmptyMealName : Validation()
+        data object EmptyDataException : Validation()
+
     }
 
     sealed class ParsingException() : FoodMoodException() {
