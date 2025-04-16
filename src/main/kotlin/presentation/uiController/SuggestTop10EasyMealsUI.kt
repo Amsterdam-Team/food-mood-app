589f7d7548cdf64f9b338f68a93c6d924bd2be82
raouf-kamel <raoufkamel2013@gmail.com>
2025-04-17 00:02:33 +0300

    refactor: rename SuggestEasyMealsUseCase and UI to reflect top 10 logic

--- src/main/kotlin/presentation/uiController/SuggestTop10EasyMealsUI.kt
+++ src/main/kotlin/presentation/uiController/SuggestTop10EasyMealsUI.kt
@@ -1,21 +1,21 @@
 package presentation.uiController
 
 import logic.models.Meal
-import logic.usecase.SuggestEasyMealsUseCase
+import logic.usecase.SuggestTop10EasyMealsUseCase
 import presentation.utils.tryToExecute
 import presentation.utils.withGreenColor
 
-class SuggestEasyMealsUI(
-    private val suggestEasyMealsUseCase: SuggestEasyMealsUseCase
+class SuggestTop10EasyMealsUI(
+    private val suggestTop10EasyMealsUseCase: SuggestTop10EasyMealsUseCase
 ) : BaseUIController {
     override fun execute() {
         tryToExecute(
-            action = suggestEasyMealsUseCase::suggestEasyMeals,
-            onSuccess = ::outputSuggestEasyMeals
+            action = suggestTop10EasyMealsUseCase::suggestEasyMeals,
+            onSuccess = ::outputsuggestTop10EasyMeals
         )
     }
 
-    private fun outputSuggestEasyMeals(meals: List<Meal>) {
+    private fun outputsuggestTop10EasyMeals(meals: List<Meal>) {
         println("Suggest Easy Meal ".withGreenColor())
         meals.forEachIndexed { index, meal ->
             println("${index + 1} - Meal Name: ${meal.name},   ".withGreenColor())
