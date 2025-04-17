17bde7afbae81983d4a5615bcc56a8e46f577231
mina0maher <mahermena160@gmail.com>
2025-04-17 03:02:25 +0200

    use base ui controller

--- src/main/kotlin/presentation/GymHelperUI.kt
+++ src/main/kotlin/presentation/GymHelperUI.kt
@@ -1,25 +1,31 @@
 package presentation
 
 import logic.usecase.SearchByCaloriesAndProteinUseCase
-import org.example.logic.EmptyDataException
+import presentation.uiController.BaseUIController
+import presentation.utils.tryToExecute
+import presentation.utils.withGreenColor
 
-class GymHelperUI(private val searchByCaloriesAndProteinUseCase: SearchByCaloriesAndProteinUseCase) {
-    fun start() {
+class GymHelperUI(private val searchByCaloriesAndProteinUseCase: SearchByCaloriesAndProteinUseCase): BaseUIController {
+
+    override fun execute() {
+        tryToExecute (
+            action = {startGymHelper()},
+            onSuccess = {
+                println("enjoy your meal!".withGreenColor())
+            }
+        )
+    }
+
+    fun startGymHelper() {
         println("Hi, Welcome to your Gym Helper!")
-
         val calories = readValidInt("Please enter your desired calories:")
         val protein = readValidInt("Please enter your desired protein:")
-
         val meals = searchByCaloriesAndProteinUseCase.getMealByCaloriesAndProtein(calories, protein)
-
-        if (meals.isNotEmpty()) {
-            println("Here are the meals that match your desired calories and protein:")
-            meals.forEach { println("Meal: ${it.name}, Calories: ${it.nutrition?.calories}, Protein: ${it.nutrition?.protein}") }
-        } else {
-            throw EmptyDataException()
+        meals.forEach {
+            println("Meal: ${it.name}," +
+                    " Calories: ${it.nutrition?.calories}," +
+                    " Protein: ${it.nutrition?.protein}")
         }
-
-
     }
 
     fun readValidInt(prompt: String): Int {
@@ -30,4 +36,6 @@ class GymHelperUI(private val searchByCaloriesAndProteinUseCase: SearchByCalorie
             println("Invalid input. Please enter a valid number.")
         }
     }
+
+
 }
