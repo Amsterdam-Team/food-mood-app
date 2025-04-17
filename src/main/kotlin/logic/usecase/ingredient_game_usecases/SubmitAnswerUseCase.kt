c1d662396c76ce5fceb0ea52e125d6a5e00d2d89
Nada AL Jarrah <nadajarrahfl20@gmail.com>
2025-04-17 16:25:31 +0200

    Add full logic and CLI interface for Ingredient Guessing Game

---
+++ src/main/kotlin/logic/usecase/ingredient_game_usecases/SubmitAnswerUseCase.kt
@@ -0,0 +1,11 @@
+package logic.usecase.ingredient_game_usecases
+
+import logic.models.IngredientGameState
+
+class SubmitAnswerUseCase {
+    fun submit(userAnswer: String, correctAnswer: String, gameState: IngredientGameState): Boolean {
+        val isCorrect = userAnswer.equals(correctAnswer, ignoreCase = true)
+        if (isCorrect) gameState.increaseScore()
+        return isCorrect
+    }
+}
