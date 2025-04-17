d750d39f4164a65c33783a03bc8eb09bf8735e19
3bnaby <mahmoud.mohamad68@gmail.com>
2025-04-17 09:37:44 +0200

    add the logic of suggest meals by calories use case

---
+++ src/main/kotlin/logic/usecase/SuggestAMealByCaloriesUseCase.kt
@@ -0,0 +1,22 @@
+package logic.usecase
+
+import logic.MealsRepository
+import logic.exception.FoodMoodException
+import logic.models.Meal
+import org.example.presentation.utils.getRandomElementOrNull
+
+class SuggestAMealByCaloriesUseCase(private val mealsRepository: MealsRepository) {
+        private val inputCalories = 700.0
+        private val filteredMealByCalories:List<Meal> by lazy {
+            mealsRepository.getAllMeals().filter {
+                (it.nutrition?.calories ?: 0.0) > inputCalories
+            }
+        }
+
+    fun chooseMealRandomly():Meal {
+        return filteredMealByCalories.ifEmpty {
+            throw FoodMoodException.Validation.EmptyDataException }
+            .getRandomElementOrNull() ?: throw FoodMoodException.Validation.EmptyDataException
+    }
+
+}
