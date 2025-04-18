8eaca7ff556ff87efba37969cb024b98e98af89c
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-04-18 19:09:12 +0200

    added iraqi and italian ui controller

--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -3,14 +3,9 @@ package presentation
 import CSVFoodParser
 import data.MealsRepositoryImpl
 import logic.GetFastHealthyMealsUseCase
-import logic.usecase.GetKetoMealsUseCase
-import logic.usecase.GetSeafoodMealsByProteinUseCase
-import logic.usecase.GuessPreparationTimeUseCase
+import logic.usecase.*
 import org.example.data.CSVFoodFileReader
-import presentation.uiController.FastHealthyMealsUIController
-import presentation.uiController.GuessGameUIController
-import presentation.uiController.MainMenuHandler
-import presentation.uiController.SeafoodMealsSuccessUIController
+import presentation.uiController.*
 import java.io.File
 
 fun main() {
@@ -31,12 +26,19 @@ fun main() {
 
     val getKetoMealsUseCase = GetKetoMealsUseCase(mealsRepositoryImpl)
 
+    val getIraqiMealsUseCase = GetIraqiMealsUseCase(mealsRepositoryImpl)
+    val iraqiMealUIController = IraqiMealUIController(getIraqiMealsUseCase)
+
+    val getItalianMealsForLargeGroupsUseCase = GetItalianMealsForLargeGroupsUseCase(mealsRepositoryImpl)
+    val italianMealUIController = ItalianMealUIController(getItalianMealsForLargeGroupsUseCase)
 
     val handlers = mapOf(
         1 to fastHealthyMealsUiController,
+        3 to iraqiMealUIController,
         5 to guessGameUIController,
         7 to KetoMealHelperUIController(getKetoMealsUseCase),
-        14 to seafoodMealsSuccessUIController
+        14 to seafoodMealsSuccessUIController,
+        15 to italianMealUIController
     )
 
     MainMenuHandler(handlers).start()
