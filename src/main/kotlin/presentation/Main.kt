a60abfef07ace2d97e0d2ddd810f301b84598e2f
omer1998 <omerfaris11@gmail.com>
2025-04-18 17:59:47 +0300

    refactor and fix import issues

--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -1,19 +1,22 @@
 package presentation
 
-import data.CSVMealsRepository
+import CSVFoodParser
 import logic.usecase.GetKetoMealsUseCase
 import data.MealsRepositoryImpl
 import logic.usecase.GetSeafoodMealsByProteinUseCase
 import logic.usecase.GuessPreparationTimeUseCase
+import org.example.data.CSVFoodFileReader
 import presentation.uiController.GuessGameUIController
 import presentation.uiController.MainMenuHandler
 import presentation.uiController.SeafoodMealsSuccessUIController
 import java.io.File
 
 fun main() {
-    val meals = MealsRepositoryImpl(File("food.csv")).getAllMeals()
     val csvFile = File("food.csv")
-    val mealsRepositoryImpl = MealsRepositoryImpl(csvFile)
+    val mealsRepositoryImpl = MealsRepositoryImpl(CSVFoodParser(CSVFoodFileReader(csvFile)))
+
+    val meals = MealsRepositoryImpl(CSVFoodParser(CSVFoodFileReader(csvFile))).getAllMeals()
+
 
     val getGuessPreparationTimeUseCase = GuessPreparationTimeUseCase(mealsRepositoryImpl)
     val guessGameUIController = GuessGameUIController(getGuessPreparationTimeUseCase)
@@ -21,12 +24,12 @@ fun main() {
     val seafoodMealsSuccessUIController =
         SeafoodMealsSuccessUIController(getSeafoodMealsByProteinUseCase)
 
-    val getKetoMealsUseCase = GetKetoMealsUseCase(csvMealsRepository)
+    val getKetoMealsUseCase = GetKetoMealsUseCase(mealsRepositoryImpl)
 
 
     val handlers = mapOf(
         14 to seafoodMealsSuccessUIController,
-        7 to KetoMealHelperUIController(getKetoMealsUseCase)
+        7 to KetoMealHelperUIController(getKetoMealsUseCase),
         5 to guessGameUIController,
         14 to seafoodMealsSuccessUIController
     )
