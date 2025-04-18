bc191af7e2fcdc2ffedb5f124741b2331ecd049c
raouf-kamel <raoufkamel2013@gmail.com>
2025-04-18 19:10:56 +0300

    add validation to meals ifEmpty

--- src/main/kotlin/logic/usecase/SuggestTop10EasyMealsUseCase.kt
+++ src/main/kotlin/logic/usecase/SuggestTop10EasyMealsUseCase.kt
@@ -1,13 +1,17 @@
 package logic.usecase
 
 import logic.MealsRepository
+import logic.exception.FoodMoodException
 import logic.models.Meal
 
 class SuggestTop10EasyMealsUseCase(
     private val mealsRepository: MealsRepository
 ) {
     fun suggestEasyMeals(): List<Meal> {
-        return mealsRepository.getAllMeals().filter(::onSuggestEasyMeals).shuffled().take(10)
+        val allMeals = mealsRepository.getAllMeals()
+        val validMeals = allMeals.ifEmpty { throw FoodMoodException.Validation.EmptyDataException }
+
+        return validMeals.filter(::onSuggestEasyMeals).shuffled().take(10)
     }
     private fun onSuggestEasyMeals(meal: Meal): Boolean {
         return (meal.preparationTime ?: Int.MAX_VALUE) <= 30 &&
