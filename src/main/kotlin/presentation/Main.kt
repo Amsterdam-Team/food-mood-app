1f955b6ec419f4b8168bb8aac7a80114ccada245
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-04-18 17:18:41 +0300

    add FastHealthyMealsUIController to the MainMenuHandler map

--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -1,8 +1,10 @@
 package presentation
 
 import data.MealsRepositoryImpl
+import logic.GetFastHealthyMealsUseCase
 import logic.usecase.GetSeafoodMealsByProteinUseCase
 import logic.usecase.GuessPreparationTimeUseCase
+import presentation.uiController.FastHealthyMealsUIController
 import presentation.uiController.GuessGameUIController
 import presentation.uiController.MainMenuHandler
 import presentation.uiController.SeafoodMealsSuccessUIController
@@ -15,16 +17,16 @@ fun main() {
 
     val getGuessPreparationTimeUseCase = GuessPreparationTimeUseCase(mealsRepositoryImpl)
     val guessGameUIController = GuessGameUIController(getGuessPreparationTimeUseCase)
-    val getSeafoodMealsByProteinUseCase = GetSeafoodMealsByProteinUseCase(mealsRepositoryImpl)
-    val seafoodMealsSuccessUIController =
-        SeafoodMealsSuccessUIController(getSeafoodMealsByProteinUseCase)
 
-    val getSeafoodMealsByProteinUseCase = GetSeafoodMealsByProteinUseCase(csvMealsRepository)
+    val fastHealthyMealsUseCase = GetFastHealthyMealsUseCase(mealsRepositoryImpl)
+    val fastHealthyMealsUiController = FastHealthyMealsUIController(fastHealthyMealsUseCase)
+
+    val getSeafoodMealsByProteinUseCase = GetSeafoodMealsByProteinUseCase(mealsRepositoryImpl)
     val seafoodMealsSuccessUIController = SeafoodMealsSuccessUIController(getSeafoodMealsByProteinUseCase)
 
     val handlers = mapOf(
-        5 to guessGameUIController,
         1 to fastHealthyMealsUiController,
+        5 to guessGameUIController,
         14 to seafoodMealsSuccessUIController
     )
 
