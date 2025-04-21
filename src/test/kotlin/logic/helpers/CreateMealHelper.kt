b1c0b428cfd3e7bfb901d2b2ace0700507092610
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-04-21 22:05:37 +0200

    update createmeal function

--- src/test/kotlin/logic/helpers/CreateMealHelper.kt
+++ src/test/kotlin/logic/helpers/CreateMealHelper.kt
@@ -1,22 +1,33 @@
 package logic.helpers
 
+import kotlinx.datetime.LocalDate
 import logic.models.Meal
+import logic.models.Nutrition
 
 fun createMeal(
-    name: String?,
-    tags: List<String>?,
-    description: String?
+    name: String? = null,
+    tags: List<String>? = null,
+    description: String? = null,
+    nutrition: Nutrition? = null,
+    id: String? = null,
+    contributorId: String? = null,
+    numberOfIngredients: Int? = null,
+    numberOfSteps: Int? = null,
+    submittedDate: LocalDate? = null,
+    steps: List<String>? = null,
+    ingredients: List<String>? = null,
+    preparationTime: Int? = null
 ) = Meal(
-    id = null,
+    id = id,
     name = name,
     tags = tags,
     description = description,
-    contributorId = null,
-    nutrition = null,
-    numberOfIngredients = null,
-    numberOfSteps = null,
-    submittedDate = null,
-    steps = null,
-    ingredients = null,
-    preparationTime = null
+    contributorId = contributorId,
+    nutrition = nutrition,
+    numberOfIngredients = numberOfIngredients,
+    numberOfSteps = numberOfSteps,
+    submittedDate = submittedDate,
+    steps = steps,
+    ingredients = ingredients,
+    preparationTime = preparationTime
 )
