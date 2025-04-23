7babb0ae10fa96694f42979178678eeefa1d4800
omer1998 <omerfaris11@gmail.com>
2025-04-23 18:28:37 +0300

    refactor reading file functionality to throw appropriate exception

--- src/main/kotlin/data/CSVFoodFileReader.kt
+++ src/main/kotlin/data/CSVFoodFileReader.kt
@@ -2,19 +2,18 @@ package data
 
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.doyaaaaaken.kotlincsv.util.MalformedCSVException
+import logic.exception.FoodMoodException.ParsingException.*
 import java.io.File
 
+
 class CSVFoodFileReader(val csvFile: File) {
-
     fun readFile(): List<Map<String,String>>{
-        return try{
+        val rows = try{
             csvReader().readAllWithHeader(csvFile)
-        } catch(e: MalformedCSVException){
-            println("malformed CSV file")
-            emptyList<Map<String,String>>()
-        } catch (e:Exception){
-            emptyList<Map<String,String>>()
         }
-    }
-
+        catch(e: MalformedCSVException){
+            throw MalFormedCsvFileException
+        }
+        if (rows.isEmpty()) throw EmptyFileException
+        return rows }
 }
