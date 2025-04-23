d74eb8a32a1d632dec542552098921a7fbf72836
omer1998 <omerfaris11@gmail.com>
2025-04-23 13:27:50 +0300

    refactor file reader to be return list of map representing rows of thefile

--- src/main/kotlin/data/CSVFoodFileReader.kt
+++ src/main/kotlin/data/CSVFoodFileReader.kt
@@ -1,11 +1,20 @@
 package data
 
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import com.github.doyaaaaaken.kotlincsv.util.MalformedCSVException
 import java.io.File
 
 class CSVFoodFileReader(val csvFile: File) {
 
-    fun readFile(): List<String>{
-        return csvFile.readLines()
+    fun readFile(): List<Map<String,String>>{
+        return try{
+            csvReader().readAllWithHeader(csvFile)
+        } catch(e: MalformedCSVException){
+            println("malformed CSV file")
+            emptyList<Map<String,String>>()
+        } catch (e:Exception){
+            emptyList<Map<String,String>>()
+        }
     }
 
 }
