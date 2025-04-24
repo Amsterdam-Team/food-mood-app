01203b32243d5ad10ee74fc7254ba7a4548bcb0d
HendSayed25 <hendsayed853@gmail.com>
2025-04-24 16:18:41 +0200

    add more tests for get meal by name ise case and test to UI Controller

--- src/test/kotlin/logic/usecase/GetMealByNameUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/GetMealByNameUseCaseTest.kt
@@ -4,7 +4,9 @@ import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import logic.MealsRepository
-import logic.exception.FoodMoodException
+import logic.exception.FoodMoodException.Validation.NotFoundMealName
+import logic.helpers.allInTheKitchen
+import logic.helpers.arribaBakedWinter
 import logic.helpers.createMeal
 import logic.search.SearchUsingKMP
 import org.junit.jupiter.api.BeforeEach
@@ -13,22 +15,22 @@ import org.junit.jupiter.api.assertThrows
 
 class GetMealByNameUseCaseTest {
 
-    private lateinit var getMealsByNameUseCase: GetMealByNameUseCase
-    private lateinit var mealRepository: MealsRepository
+    private lateinit var useCase: GetMealByNameUseCase
+    private lateinit var repository: MealsRepository
     private lateinit var searchUsingKMP: SearchUsingKMP
 
     @BeforeEach
     fun setUp() {
         searchUsingKMP = mockk(relaxed = true)
-        mealRepository = mockk(relaxed = true)
-        getMealsByNameUseCase = GetMealByNameUseCase(searchUsingKMP, mealRepository)
+        repository = mockk(relaxed = true)
+        useCase = GetMealByNameUseCase(searchUsingKMP, repository)
     }
 
     @Test
     fun `should throw NotFoundMealName when name of meal not found`() {
         //given
         val mealName = "nn"
-        every { mealRepository.getAllMeals() } returns listOf(
+        every { repository.getAllMeals() } returns listOf(
             createMeal(name = "bahraini sweet rice  muhammar"),
             createMeal(name = "chocolate  cherry pecan biscotti")
         )
@@ -40,8 +42,8 @@ class GetMealByNameUseCaseTest {
         } returns null
 
         //when && throw
-        assertThrows<FoodMoodException.Validation.NotFoundMealName> {
-            getMealsByNameUseCase.getMealDetails(mealName)
+        assertThrows<NotFoundMealName> {
+            useCase.getMealDetails(mealName)
         }
 
     }
@@ -50,7 +52,7 @@ class GetMealByNameUseCaseTest {
     fun `should return details of meal when the name of meal is found`() {
         //given
         val mealName = "bahraini sweet rice  muhammar"
-        every { mealRepository.getAllMeals() } returns listOf(
+        every { repository.getAllMeals() } returns listOf(
             createMeal(name = "bahraini sweet rice  muhammar", description = "good meals"),
             createMeal(name = "chocolate  cherry pecan biscotti", description = "bad meal")
         )
@@ -62,11 +64,86 @@ class GetMealByNameUseCaseTest {
         } returns mealName
 
         //when
-        val mealDetails = getMealsByNameUseCase.getMealDetails(mealName)
+        val mealDetails = useCase.getMealDetails(mealName)
 
         //then
         assertThat(mealDetails).containsExactly(
             createMeal(name = "bahraini sweet rice  muhammar", description = "good meals"),
         )
+    }
+    @Test
+    fun `should return details of meal when meal name is found but with missing characters from end of name`() {
+        val mealName = "all in the kitchen  chi"
+        val correctName = "all in the kitchen  chili"
+        val meals = listOf(
+            arribaBakedWinter(),
+            allInTheKitchen()
+        )
+
+        every { repository.getAllMeals() } returns meals
+        every {
+            searchUsingKMP.validateTheInputInExistData(mealName, meals.mapNotNull { it.name })
+        } returns correctName
+
+        val mealDetails = useCase.getMealDetails(mealName)
+
+        assertThat(mealDetails).contains(allInTheKitchen())
+    }
+
+    @Test
+    fun `should return details of meal when meal name is found but with missing characters from beginning of name`() {
+        val mealName = " in the kitchen  chi"
+        val correctName = "all in the kitchen  chili"
+        val meals = listOf(
+            arribaBakedWinter(),
+            allInTheKitchen()
+        )
+
+        every { repository.getAllMeals() } returns meals
+        every {
+            searchUsingKMP.validateTheInputInExistData(mealName, meals.mapNotNull { it.name })
+        } returns correctName
+
+        val mealDetails = useCase.getMealDetails(mealName)
+
+        assertThat(mealDetails).contains(allInTheKitchen())
+    }
+
+    @Test
+    fun `should return details of meal when meal name is found but with missing characters from middle of name`() {
+        val mealName = "all in  kitchen  chi"
+        val correctName = "all in the kitchen  chili"
+        val meals = listOf(
+            arribaBakedWinter(),
+            allInTheKitchen()
+        )
+
+        every { repository.getAllMeals() } returns meals
+        every {
+            searchUsingKMP.validateTheInputInExistData(mealName, meals.mapNotNull { it.name })
+        } returns correctName
+
+        val mealDetails = useCase.getMealDetails(mealName)
+
+        assertThat(mealDetails).contains(allInTheKitchen())
+    }
+
+    @Test
+    fun `should return details of meal when meal name is found but the name is reversed`() {
+        val mealName = "chilli kitchen the in all"
+        val correctName = "all in the kitchen  chili"
+        val meals = listOf(
+            arribaBakedWinter(),
+            allInTheKitchen()
+        )
+
+        every { repository.getAllMeals() } returns meals
+        every {
+            searchUsingKMP.validateTheInputInExistData(mealName, meals.mapNotNull { it.name })
+        } returns correctName
+
+        val mealDetails = useCase.getMealDetails(mealName)
+
+        assertThat(mealDetails).contains(allInTheKitchen())
     }
 }
